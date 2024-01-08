18b22b949ed912564ef6405f8dbd38565c88c5e3
Franco Pietrokovsky <Francodavidpietrokovsky@gmail.com>
2024-01-08 13:59:20 -0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,13 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "https://passgen-p1etrodev.netlify.app");
+  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.post("/password", (req, res) => {
 	const len = Number(req.body.length);
 	const useData = Object(req.body.useData);
